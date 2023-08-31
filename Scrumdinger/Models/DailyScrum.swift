f34e11e21625e246ed0eb467f5aa2e9465f48bf1
devjangir <devpassion7@gmail.com>
2023-08-31 18:43:15 +0530

    DetailView Edit view create, and link with detail view

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -12,6 +12,14 @@ struct DailyScrum: Identifiable {
     var title: String
     var attendees: [Attendee]
     var lengthInMinutes: Int
+    var lengthInMinutesAsDouble: Double {
+        get {
+            Double(lengthInMinutes)
+        }
+        set {
+            lengthInMinutes = Int(newValue)
+        }
+    }
     var theme: Theme
     
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
@@ -32,11 +40,14 @@ extension DailyScrum {
             self.name = name
         }
     }
+    
+    static var emptyScrum: DailyScrum {
+        DailyScrum(title: "", attendees: [], lengthInMinutes: 5, theme: .sky)
+    }
 }
 extension DailyScrum {
     static let sampleData: [DailyScrum] = [
-        DailyScrum(title: "Design Data
-                           ",
+        DailyScrum(title: "Design Data",
                            attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
                            lengthInMinutes: 10,
                            theme: .yellow),
